bc48c12716a8d40dc01e4024643c6997a07fbcaa
Chris <chris.james.davis@gmail.com>
2021-04-10 18:08:51 -0700

    removed unused variable

--- lib/web_scrapper.py
+++ lib/web_scrapper.py
@@ -5,7 +5,6 @@ class WebScraper:
 
     def __init__(self):
         self.eco_url = 'https://wiki.play.eco/en/Crafting'
-        self.items = {'items': []}
 
     def scrape(self):
         req = Request(self.eco_url, headers={'User-Agent': 'Magic Browser'})
