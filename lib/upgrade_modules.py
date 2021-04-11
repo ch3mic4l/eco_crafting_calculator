e3e79311a8dd583e2a89859f6ae0b6df87382bf7
Chris <chris.james.davis@gmail.com>
2021-04-11 11:12:33 -0700

    added functions to calculate the crafting time and materials

--- lib/upgrade_modules.py
+++ lib/upgrade_modules.py
@@ -1,3 +1,6 @@
+from sys import exit
+
+
 class UpgradeModules:
 
     def __init__(self):
@@ -28,3 +31,28 @@ class UpgradeModules:
             'four': 4,
             'five': 5
         }
+
+    def calculate_material_with_upgrades(self, level: int, quantity: (int, float)):
+        if level == 0:
+            return quantity
+        upgrade_module = self.find_upgrade_module(level)
+        resource_cost = self.__getattribute__(upgrade_module)['resource_cost']
+        return quantity - ((quantity / 100) * resource_cost)
+
+    def calculate_crafting_time_with_upgrades(self, level: int, crafting_time: (int, float)):
+        if level == 0:
+            return crafting_time
+        upgrade_module = self.find_upgrade_module(level)
+        resource_cost = self.__getattribute__(upgrade_module)['craft_time']
+        return crafting_time - ((crafting_time / 100) * resource_cost)
+
+    def find_upgrade_module(self, level: int):
+        upgrade_level = ''
+        for module_level in self.lookup_table:
+            if self.lookup_table[module_level] == level:
+                upgrade_level = module_level
+                break
+        if upgrade_level == '':
+            print("Module {} not found".format(level))
+            exit(102)
+        return upgrade_level
